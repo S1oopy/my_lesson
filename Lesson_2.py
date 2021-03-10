2dab03710979f857f22a0834956a2d5db28a995a
s1oopy <onishenko.19999@gmail.com>
2021-03-10 18:13:13 +0300

    Добавил площадь 4х угольника

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,4 +1,4 @@
-def pl_treyg(a,b):
+def pl_3_yg(a,b):
     if a > 0 and b > 0:
         return (a+b)/2
 
@@ -6,4 +6,7 @@ def pl_trup(a, b, h):
     if a > 0 and b > 0 and h > 0:
         return ((a+b)*h)/2
     
-    
+def pl_4_yg(a, b):
+    if a > 0 and b > 0:
+        return (a+b)*2
+         
